872b5e6633ca200ce3fda0ae0c462893c63cc737
Vineet Gupta <vineet091@users.noreply.github.com>
2021-08-17 16:36:03 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
 import "./styles.css";
 import React, { useState } from "react";
-
+const seatsCatogory = [
+  {
+    name: "Club",
+    price: 236,
+    seats: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    occupied: [2, 3]
+  },
+  {
+    name: "Executive",
+    price: 200,
+    seats: [
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+      30
+    ],
+    occupied: [26]
+  }
+];
 export default function App() {
-  const [seatsCatogory, setSeatsCatogory] = useState([
-    {
-      name: "Club",
-      price: 236,
-      seats: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-      occupied: [2, 3]
-    },
-    {
-      name: "Executive",
-      price: 200,
-      seats: [
-        11,
-        12,
-        13,
-        14,
-        15,
-        16,
-        17,
-        18,
-        19,
-        20,
-        21,
-        22,
-        23,
-        24,
-        25,
-        26,
-        27,
-        28,
-        29,
-        30
-      ],
-      occupied: [26]
-    }
-  ]);
   const [selectedSeats, setSelectedSeats] = useState([]);
   const [selectedCatogory, setSelectedCatogory] = useState(null);
 
